933d3185219a3b269d75e19a80b7fb5ff2f0cca4
rushi6457 <rushiypatil@gmail.com>
2023-02-01 23:07:18 +0530

    add

--- client/grow-calculator/pages/calculate.tsx
+++ client/grow-calculator/pages/calculate.tsx
@@ -34,24 +34,41 @@ ChartJS.register(ArcElement, Tooltip, Legend);
 
 function Calculate(){
 
-    const [state,setState] = useState(500)
-    const [state1,setState1] = useState(15)
-    const dist=2000
-    const interest = 0.071
+  const [investment,setInvestment] = useState({
+      yearlyInvestment:500,
+      timePeriod:15,
+      rateOfInterest:7.1
+  })
+
+  // const calculateMatuity =async () =>{
+
+      // let res = await axios.post('http://localhost:5000/calculate',investment)
+      // console.log(res);
+      const investmentAmount = investment.yearlyInvestment * investment.timePeriod;
+    const totalInterest =
+      investmentAmount * investment.rateOfInterest * investment.timePeriod;
+    const maturityValue = investmentAmount + totalInterest;
+    // return [ investmentAmount, totalInterest, maturityValue ];
+      
+  // }
+
+  // useEffect(()=>{
+
+    // calculateMatuity()
+  // },[ investmentAmount, totalInterest, maturityValue])
+    // const [state,setState] = useState(500)
+    // const [state1,setState1] = useState(15)
+    // const dist=2000
+    // const interest = 0.071
     
-    const investedAmount = state * state1
-    // console.log(investedAmount)
-    const [getState,setGetState] = useState({
-      investedAmount,
-      interest,
-      state1
-    })
+    // const investedAmount = state * state1
+    
 const data = {
-  labels: [interest,state1],
+  labels: [investmentAmount,totalInterest],
   datasets: [
     {
       label: 'Total investment',
-      data: [state,interest],
+      data: [investmentAmount,totalInterest],
       backgroundColor: [
         '#2828FF',
         'rgba(54, 162, 235, 0.2)',
@@ -64,23 +81,18 @@ const data = {
     },
   ],
 };
+
+const handleChange = (e:any) =>{
+  const {name,value} = e.target
+      setInvestment({
+        ...investment,
+        [name]:value
+      })
+}
+console.log(investment);
+
  
 
-  
-
-    useEffect(()=>{
-        getGraph(state,state1,interest)
-    },[state,state1])
-
-    const handleChange = (e:any) =>{
-        setState(e.target.value)
-        // setGetState(e.target.value)
-    }
-
-    const handleChange1 = (e:any) =>{
-        setState1(e.target.value)
-        // setGetState(e.target.value)
-    }
     
     return (
         <div>
@@ -92,27 +104,27 @@ const data = {
         <Container>
             <Flex alignItems={'center'} justifyContent={'space-between'}>
                 <Text fontSize={'25px'}>Yearly Investment</Text>
-                <Box>RS: {state}</Box>
+                <Box>RS: {investment.yearlyInvestment}</Box>
             </Flex>
-          <input onChange={handleChange} type="range" max='50000' min='500' />
+        <input onChange={handleChange} value={investment.yearlyInvestment} name='yearlyInvestment' type="range" min={'500'} max='50000'  />
         </Container>
 
          <Container>
              <Flex alignItems={'center'} justifyContent={'space-between'}>
                 <Text fontSize={'25px'}>Time period(in years)</Text>
-                <Box> {`${state1}Yr`}</Box>
+                <Box> {`${investment.timePeriod}Yr`}</Box>
             </Flex>
-          <input onChange={handleChange1} type="range" max='50' min='15' />
+          <input onChange={handleChange} value={investment.timePeriod} name='timePeriod' type="range" max='50' min='15' />
         </Container>
 
        <Flex justifyContent={'justify'} gap={'10px'}>
          <Text>Rate of interest</Text>
-         <Box>{`${interest}%`}</Box>
+         <Box>{`${investment.rateOfInterest}%`}</Box>
        </Flex>
 
-          <Text>{`Invested Amount: ${investedAmount}`}</Text>
-          <Text>{`Total Interest: $`}</Text>
-          <Text>{`Maturity value: $`}</Text>
+          <Text>{`Invested Amount: ${Math.floor(investmentAmount)}`}</Text>
+          <Text>{`Total Interest: ${Math.floor(totalInterest)}`}</Text>
+          <Text>{`Maturity value: ${Math.floor(maturityValue)}`}</Text>
     </Box>
    
         <Box>
