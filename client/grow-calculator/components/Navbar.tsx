f88d3cd4c685ac05b8f2b76b5bc5d18cccc96f45
rushi6457 <rushiypatil@gmail.com>
2023-02-01 17:17:38 +0530

    add

--- client/grow-calculator/components/Navbar.tsx
+++ client/grow-calculator/components/Navbar.tsx
@@ -1,5 +1,5 @@
 import { logout } from "@/redux/auth/actions";
-import { Button, Flex, ListItem, UnorderedList } from "@chakra-ui/react"
+import { Button, Flex, Heading, ListItem, UnorderedList } from "@chakra-ui/react"
 import Link from "next/link"
 import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
@@ -9,7 +9,7 @@ import { useDispatch, useSelector } from "react-redux"
 function Navbar () {
 
     const auth = useSelector((store:any)=>store.auth)
-    console.log(auth);
+    // console.log(auth);
     const router = useRouter()
     const [state,setState] = useState(true)
     const dispatch = useDispatch()
@@ -34,14 +34,15 @@ const handleClick = () =>{
             <Flex justifyContent={'space-between'} alignItems={'center'}  padding={'10px'} bgColor={'blue.200'}  width={'100%'}>
                     <UnorderedList textStyle={'none'} listStyleType={'none'}>
                          <Flex justifyContent={'space-around'} gap={'50px'}>
-                            <ListItem fontSize={'2xl'} fontWeight={'600'} >
-                                <Link href={'/profile'}>Profile</Link>    
+                            {/* <ListItem fontSize={'2xl'} fontWeight={'600'} >
+                                <Link  href={'/profile'}>Profile</Link>    
                             </ListItem>    
                             <ListItem fontSize={'2xl'}  fontWeight={'600'} >
-                                <Link href={'/calculate'}>Calculate</Link>
-                            </ListItem>    
+                                <Link href={'/calculate'}>Calculate</Link> */}
+                            {/* </ListItem>     */}
                         </Flex>   
                     </UnorderedList>
+                    <Heading>Grow Calculator</Heading>
                 <Flex justifyContent={'space-around'} gap={'20px'} padding={'10px'}>
                     <Link href='/signup'>
                         <Button variant={'solid'} colorScheme={'red'}>Signup</Button>
--- client/grow-calculator/pages/calculate.tsx
+++ client/grow-calculator/pages/calculate.tsx
@@ -11,72 +11,105 @@ import {
   Box,
   Text,
   Input,
-} from '@chakra-ui/react'
+} from '@chakra-ui/react';
+import { Pie } from 'react-chartjs-2';
+import { Chart as ChartJS, ArcElement, Tooltip, Legend } from 'chart.js/auto';
 import axios from "axios";
 import {useEffect,useState} from "react";
+ChartJS.register(ArcElement, Tooltip, Legend);
 
-const  getGraph =  async() => {
-    let res = await axios.get('http://localhost:5000/calculate')
-    // console.log(res);
-             
-}
 
 
 function Calculate(){
 
-    const [state,setState] = useState(0)
-    const [state1,setState1] = useState(0)
-    const interest = 0.070
-    // const [userData,setUserData] =  useState({
-    //     labels:state,
-    //     datasets:[{
-    //         label:"Total Investment",
-    //         data:state1,
-    //         backgroundColor:["skyblue","green"]
-    //     }]
-    // })
+    const [state,setState] = useState(500)
+    const [state1,setState1] = useState(15)
+  
+    const interest = 0.071
+    const investedAmount = state * state1
+    // console.log(investedAmount)
+    const [getState,setGetState] = useState({
+      investedAmount,
+      interest,
+      state1
+    })
+const data = {
+  labels: [state1],
+  datasets: [
+    {
+      label: 'Total investment',
+      data: [state,interest],
+      backgroundColor: [
+        'rgba(255, 99, 132, 0.2)',
+        'rgba(54, 162, 235, 0.2)',
+      ],
+      borderColor: [
+        'rgba(255, 99, 132, 1)',
+        'rgba(54, 162, 235, 1)',
+      ],
+      borderWidth: 1,
+    },
+  ],
+};
+ 
+
+    const  getGraph =  async() => {
+    let res = await axios.post('http://localhost:5000/calculate',investedAmount)
+    console.log(res);
+             
+}
+
     useEffect(()=>{
         getGraph()
     },[])
 
     const handleChange = (e:any) =>{
         setState(e.target.value)
-        
+        setGetState(e.target.value)
     }
 
     const handleChange1 = (e:any) =>{
         setState1(e.target.value)
+        setGetState(e.target.value)
     }
     
     return (
         <div>
         <Navbar/>
            
-    <Flex>
+    <Flex justifyContent={'space-around'} alignItems={'center'}>
         
-        <Box>
+      <Box>
         <Container>
             <Flex alignItems={'center'} justifyContent={'space-between'}>
                 <Text fontSize={'25px'}>Years Investment</Text>
                 <Box>RS: {state}</Box>
             </Flex>
-          <input onChange={handleChange} type="range" max='50000' min='1' />
+          <input onChange={handleChange} type="range" max='50000' min='500' />
         </Container>
 
          <Container>
              <Flex alignItems={'center'} justifyContent={'space-between'}>
                 <Text fontSize={'25px'}>Time period(in years)</Text>
-                <Box>RS: {state1}</Box>
+                <Box> {`${state1}Yr`}</Box>
             </Flex>
-          <input onChange={handleChange1} type="range" max='15' min='1' />
+          <input onChange={handleChange1} type="range" max='50' min='15' />
         </Container>
 
-       <Flex justifyContent={'center'} gap={'10px'}>
+       <Flex justifyContent={'justify'} gap={'10px'}>
          <Text>Rate of interest</Text>
          <Box>{interest}</Box>
        </Flex>
+
+          <Text>{`Invested Amount: ${investedAmount}`}</Text>
     </Box>
-    </Flex>
+   
+        <Box>
+            <Pie data={data}/>
+        </Box>
+     </Flex>
+
+
         </div>
     )
 }
